9a988b3782f7aba9be1fc587716732b5ea8e0147
kkhatayan-cell <kkhatayan@gmail.com>
2025-12-24 23:49:46 +0400

    added jumping script

---
+++ Assets/Scripts/Jumping.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerJump : MonoBehaviour
+{
+    public float jumpForce = 7f;
+
+    private Rigidbody rigidbodyComponent;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rigidbodyComponent = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Keyboard.current.spaceKey.wasPressedThisFrame && isGrounded)
+        {
+            rigidbodyComponent.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
