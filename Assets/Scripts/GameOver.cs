c88ac92ec1d4bfd872107bd06b958141b82f50aa
ssyuzi50-dot <ssyuzi50@gmail.com>
2026-01-22 19:46:48 +0400

    update

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("LosePoint"))
+        {
+            Debug.Log("YOU LOSE!");
+
+            
+            Time.timeScale = 0f;
+
+            
+        }
+    }
+}
