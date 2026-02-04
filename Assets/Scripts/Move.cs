635bf18e02c465282ca134f7fb600f092ee30b97
l7945100-blip <l7945100@gmail.com>
2026-02-05 00:37:19 +0400

    asdfg

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -6,6 +6,7 @@ public class Move : MonoBehaviour
     SpriteRenderer sr;
 
     public float speed = 10f;
+    bool isRotated=false;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,25 +17,44 @@ public class Move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(isRotated);
         Debug.Log(Time.deltaTime);
         if (Keyboard.current.rightArrowKey.isPressed)
         {
             MoveRight();
+            isRotated=false;
+            Rotated();
+
         }
         else if (Keyboard.current.leftArrowKey.isPressed)
         {
             MoveLeft();
+            isRotated=true;
+            Rotated();
+
         }
     }
     void MoveLeft()
     {
         transform.position += new Vector3(-speed*Time.deltaTime, 0, 0);
-        sr.flipX = true;
+        
     }
     void MoveRight()
     {
         transform.position += new Vector3(speed*Time.deltaTime, 0, 0);
-        sr.flipX = false;
+        
+    }
+    void Rotated()
+    {
+        if (isRotated == true)
+        {
+            
+            transform.Rotate(0f,180f,0f);
+        }
+        else
+        {
+            transform.Rotate(0f,0f,0f);
+        }
     }
 
 }
