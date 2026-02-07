7248f49e22ec19fa4f74c757b90f18b491183b62
Telman Gurgenyan <telman.dev@gmail.com>
2026-02-07 16:27:06 +0400

    Added shoot

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -1,45 +1,17 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class Shoot : MonoBehaviour
 {
     public GameObject bulletPrefab;
-    public Transform firePoint;
     public float bulletSpeed = 10f;
-
-    private SpriteRenderer sr;
-
-    void Start()
-    {
-        sr = GetComponent<SpriteRenderer>();
-    }
 
     void Update()
     {
-        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            Fire();
-        }
-    }
-
-    void Fire()
-    {
-        GameObject bullet = Instantiate(
-            bulletPrefab,
-            firePoint.position,
-            Quaternion.identity
-        );
-
-        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
-
-        // If player is flipped → shoot LEFT
-        if (sr.flipX)
-        {
-            rb.linearVelocity = new Vector2(-bulletSpeed, 0f);
-        }
-        else // shoot RIGHT
-        {
-            rb.linearVelocity = new Vector2(bulletSpeed, 0f);
+            GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+            Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+            rb.linearVelocity = new Vector2(transform.right.x * bulletSpeed, 0);
         }
     }
 }
