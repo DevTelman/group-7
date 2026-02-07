1b4efb9fa85d878c5c91661667b3622182df24d1
Telman Gurgenyan <telman.dev@gmail.com>
2026-02-07 16:32:43 +0400

    Fixed movement

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -3,58 +3,43 @@ using UnityEngine.InputSystem;
 
 public class Move : MonoBehaviour
 {
-    SpriteRenderer sr;
-
     public float speed = 10f;
-    bool isRotated=false;
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        sr = GetComponent<SpriteRenderer>();
-    }
+    bool facingRight = true;
 
-    // Update is called once per frame
     void Update()
     {
-        Debug.Log(isRotated);
-        Debug.Log(Time.deltaTime);
         if (Keyboard.current.rightArrowKey.isPressed)
         {
             MoveRight();
-            isRotated=false;
-            Rotated();
-
+            if (!facingRight)
+            {
+                Flip();
+                facingRight = true;
+            }
         }
         else if (Keyboard.current.leftArrowKey.isPressed)
         {
             MoveLeft();
-            isRotated=true;
-            Rotated();
-
+            if (facingRight)
+            {
+                Flip();
+                facingRight = false;
+            }
         }
     }
+
     void MoveLeft()
     {
-        transform.position += new Vector3(-speed*Time.deltaTime, 0, 0);
-        
-    }
-    void MoveRight()
-    {
-        transform.position += new Vector3(speed*Time.deltaTime, 0, 0);
-        
-    }
-    void Rotated()
-    {
-        if (isRotated == true)
-        {
-            
-            transform.Rotate(0f,180f,0f);
-        }
-        else
-        {
-            transform.Rotate(0f,0f,0f);
-        }
+        transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
     }
 
+    void MoveRight()
+    {
+        transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+    }
+
+    void Flip()
+    {
+        transform.Rotate(0f, 180f, 0f);
+    }
 }
