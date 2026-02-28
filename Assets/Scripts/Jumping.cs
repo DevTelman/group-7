7b6d589d08f0381489a736b1b67d2fcd0e7267d8
l7945100-blip <l7945100@gmail.com>
2026-02-28 20:54:34 +0400

    aa

--- Assets/Scripts/Jumping.cs
+++ Assets/Scripts/Jumping.cs
@@ -4,7 +4,7 @@ using UnityEngine.InputSystem;
 public class Jump : MonoBehaviour
 {
     public float jumpForce = 10f;
-    public Animator animator;
+ 
     private Rigidbody2D rb;
     private bool isGrounded = true;
 
@@ -20,7 +20,7 @@ public class Jump : MonoBehaviour
             rb.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
             isGrounded = false;
         }
-        animator.SetBool("Jump", !isGrounded);
+       
     }
 
     void OnCollisionEnter2D(Collision2D collision)
