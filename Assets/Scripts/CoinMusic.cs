b5e29041617b9c03c7b60a0733f215bef5cab6c4
ssyuzi50-dot <ssyuzi50@gmail.com>
2026-03-01 21:55:24 +0400

    update

---
+++ Assets/Scripts/CoinMusic.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CoinPickup : MonoBehaviour
+{
+    public GameObject player;      // Քաշում ես Player-ը
+    public AudioSource coinSound;  // Քաշում ես coin-ի AudioSource
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject == player)
+        {
+            coinSound.Play();
+            Destroy(gameObject, coinSound.clip.length);
+        }
+    }
+}
