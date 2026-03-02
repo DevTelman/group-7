9f826e3814479ca3d0250ec007bbbc67efb47888
ssyuzi50-dot <ssyuzi50@gmail.com>
2026-03-02 19:46:14 +0400

    UBDATE

---
+++ Assets/Scripts/erg.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class erg : MonoBehaviour
+{
+    public AudioSource audioSource;   // Enemy-ի AudioSource
+    public GameObject player;         // Player-ը Inspector-ով կքաշես
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject == player)
+        {
+            audioSource.Play();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject == player)
+        {
+            audioSource.Play();
+        }
+    }
+}
