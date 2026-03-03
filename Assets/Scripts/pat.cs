c3baa10482bd4fee22e1627e0ce8711e24dc645c
ssyuzi50-dot <ssyuzi50@gmail.com>
2026-03-03 23:19:20 +0400

    update

---
+++ Assets/Scripts/pat.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class pat : MonoBehaviour
+{
+    private Collider2D barrierCollider;
+
+    void Start()
+    {
+        barrierCollider = GetComponent<Collider2D>();
+    }
+
+    public void OpenBarrier()
+    {
+        barrierCollider.enabled = false; // բացում է ճանապարհը
+    }
+}
